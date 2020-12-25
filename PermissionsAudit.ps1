36ace5855aa5dcc9d9ebb486af35441f93ff28c1
ElliottOnGH <>
2020-12-24 22:39:44 -0800

    Created Get-Permissions function, added verbosity and error handling

--- PermissionsAudit.ps1
+++ PermissionsAudit.ps1
@@ -1,64 +1,62 @@
-﻿$search_folder = "C:\Powershell\"
-$out_file = "C:\Powershell\Results\Temp-Temp.csv"
-$out_error = "C:\Powershell\Results\Temp-Temp-errors.csv"
+﻿[CmdletBinding()]
+Param()
 
-$found = @()
-$errors = @()
+$RootFolder = "C:\Powershell"
 
-try {
-    $acl = Get-Acl $search_folder
+$OutputFile = $($RootFolder.Split([IO.Path]::GetInvalidFileNameChars()) -join '') + ".csv"
+$ErrorFile = $($RootFolder.Split([IO.Path]::GetInvalidFileNameChars()) -join '') + ".errors.csv"
 
-    ForEach ($entry in $acl.access) {
-        $found += New-Object -TypeName PSObject -Property @{
-            Folder = $search_folder
-            Access = $entry.FileSystemRights
-            Control = $entry.AccessControlType
-            User = $entry.IdentityReference
-            Inheritance = $entry.IsInherited    
-        }        
-    }
-} catch {
-    $errors += New-Object -TypeName PSObject -Property @{
-        Item = $search_folder
-        Error = $_.exception
-    }
+$PermissionsOutput = @()
+$ErrorOutput = @()
+$FormattedOutput = @()
+
+function LogError ([string]$Path,[string]$Exception) {
+    New-Object -TypeName PSObject -Property @{
+        Path = $Path
+        Exception = $Exception
+    } | Export-Csv -NoTypeInformation -Append -Path $ErrorFile
 }
 
-cmd /c dir "$search_folder" /b /s | ForEach {
-    try {
-        $item = $_
-        $acl = Get-Acl $item
+function Get-Permissions {
+    param(
+        [string]$Path,
+        [bool]$Inheritance = $false
+    )
 
+    try {
+        $acl = Get-Acl $Path -ErrorAction Stop
+        Write-Verbose $Path
         ForEach ($entry in $acl.access) {
-            If (!$entry.IsInherited) { 
-                $found += New-Object -TypeName PSObject -Property @{
-                    Folder = $item
+            If ((!$entry.IsInherited -and !$Inheritance) -or ($Inheritance)) { 
+                New-Object -TypeName PSObject -Property @{
+                    Folder = $Path
                     Access = $entry.FileSystemRights
                     Control = $entry.AccessControlType
                     User = $entry.IdentityReference
-                    Inheritance = $entry.IsInherited    
-                }        
+                    Inheritance = $entry.IsInherited
+                }
             }
         }
     } catch {
-        $errors += New-Object -TypeName PSObject -Property @{
-            Item = $item
-            Error = $_.exception
-        }
+        LogError -Path $Path -Exception $_.exception
     }
 }
 
-$formatted = @()
-$found | Select-Object -Property Folder,Control,Access,Inheritance -Unique | ForEach-Object {
+$PermissionsOutput = $(
+    Get-Permissions -Path "$RootFolder" -Inheritance $true;
+    cmd /c dir "$RootFolder" /b /s | %{ Get-Permissions -Path "$_" }
+)
+
+$PermissionsOutput | Select-Object -Property Folder,Control,Access,Inheritance -Unique | ForEach-Object {
     $CurrentFolder = $_.Folder
     $CurrentControl = $_.Control
     $CurrentAccess = $_.Access
     $CurrentInheritance = $_.Inheritance
     $CurrentUsers = ""
-    $found | Where-Object { $_.Folder -eq $CurrentFolder -and $_.Control -eq $CurrentControl -and $_.Access -eq $CurrentAccess -and $_.Inheritance -eq $CurrentInheritance } | ForEach-Object {
+    $PermissionsOutput | Where-Object { $_.Folder -eq $CurrentFolder -and $_.Control -eq $CurrentControl -and $_.Access -eq $CurrentAccess -and $_.Inheritance -eq $CurrentInheritance } | ForEach-Object {
         $CurrentUsers = "$CurrentUsers" + $_.User + ";"
     }
-    $formatted += New-Object -TypeName PSObject -Property @{
+    $FormattedOutput += New-Object -TypeName PSObject -Property @{
         Folder = $CurrentFolder
         Access = $CurrentAccess
         Control = $CurrentControl
@@ -67,9 +65,4 @@ $found | Select-Object -Property Folder,Control,Access,Inheritance -Unique | For
     }
 }
 
-$formatted | 
-Select-Object -Property Folder,Users,Control,Access,Inheritance | 
-Export-Csv -NoTypeInformation -Path $out_file
-
-$errors |
-Export-Csv -NoTypeInformation -Path $out_error
+$FormattedOutput | Select-Object -Property Folder,Users,Control,Access,Inheritance | Export-Csv -NoTypeInformation -Path $OutputFile
